4f42b93d95778fca349d6ea43918cf23d0885676
Victor Abreu <vlabreu@outlook.com>
2021-09-13 19:43:28 -0300

    Backend project created

---
+++ backend/src/main/java/br/com/vittsistemas/dscatalog/DsCatalogVittSistemasApplication.java
@@ -0,0 +1,13 @@
+package br.com.vittsistemas.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalogVittSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalogVittSistemasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/vittsistemas/dscatalog/DsCatalogVittSistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vittsistemas.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogVittSistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
