5ebfc790fbf0d0eaddd3ba5259ac839c1ff0aeb4
Victor Abreu <vlabreu@outlook.com>
2021-10-06 11:17:54 -0300

    Basic bean validation

--- backend/src/main/java/br/com/vittsistemas/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/br/com/vittsistemas/dscatalog/dto/UserDTO.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import br.com.vittsistemas.dscatalog.entities.User;
 
 public class UserDTO implements Serializable{
@@ -13,8 +16,11 @@ public class UserDTO implements Serializable{
 
 	private Long id;
 
+	@NotBlank(message = "Campo obrigatório")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Favor entrar com um email válido!")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<>();
--- backend/src/main/java/br/com/vittsistemas/dscatalog/resources/UserResource.java
+++ backend/src/main/java/br/com/vittsistemas/dscatalog/resources/UserResource.java
@@ -2,6 +2,8 @@ package br.com.vittsistemas.dscatalog.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -46,7 +48,7 @@ public class UserResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
+	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto) {
 
 		UserDTO newdto = service.insert(dto);
 
@@ -58,7 +60,7 @@ public class UserResource {
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO dto) {
+	public ResponseEntity<UserDTO> update(@Valid @PathVariable Long id, @RequestBody UserDTO dto) {
 
 		dto = service.update(id, dto);
 
