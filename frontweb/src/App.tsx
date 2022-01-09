5b760b66e5ed2dd085471e9452b1cf0d684cdd06
Victor Abreu <vlabreu@outlook.com>
2022-01-09 15:47:41 -0300

    Finished x-small

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,14 +1,11 @@
 
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'components/Navbar';
+import Home from 'pages/Home'
 
 const App = () => {
   return(
-    <>
-    <Navbar />
-    <h1>VD Catalog</h1>
-    </>
+    <Home/>
   );
 }
 
