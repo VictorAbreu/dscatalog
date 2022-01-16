099888c5f8d75df50810a02a96e24898157012f8
Victor Abreu <vlabreu@outlook.com>
2022-01-16 11:17:03 -0300

    Home styling

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import Navbar from 'components/Navbar';
-import { ReactComponent as MainImage } from 'assets/images/main-image.svg'
-import './styles.css'
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import './styles.css';
 import ButtonIcon from 'components/ButtonIcon';
 
 const Home = () => {
@@ -10,12 +10,14 @@ const Home = () => {
       <div className="home-container">
         <div className="home-card">
           <div className="home-content-container">
-                <h1>Conheça nosso catálogo de produtos</h1>
-                <p>Ajudaremos você a encontrar os nossos melhores produtos</p>
-                <ButtonIcon/>
+            <div>
+              <h1>Conheça nosso catálogo de produtos</h1>
+              <p>Ajudaremos você a encontrar os nossos melhores produtos</p>
+            </div>
+            <ButtonIcon />
           </div>
           <div className="home-image-container">
-                <MainImage/>
+            <MainImage />
           </div>
         </div>
       </div>
