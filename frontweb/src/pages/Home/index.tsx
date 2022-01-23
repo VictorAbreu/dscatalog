b1028f3379a11bfc24d23ff830489489d9e3e792
Victor Abreu <vlabreu@outlook.com>
2022-01-23 10:47:15 -0300

    Routes

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,27 +1,26 @@
-import Navbar from 'components/Navbar';
+
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import './styles.css';
 import ButtonIcon from 'components/ButtonIcon';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <>
-      <Navbar />
       <div className="home-container">
-        <div className="home-card">
+        <div className="base-card home-card">
           <div className="home-content-container">
             <div>
               <h1>Conheça nosso catálogo de produtos</h1>
               <p>Ajudaremos você a encontrar os nossos melhores produtos</p>
             </div>
-            <ButtonIcon />
+            <Link to="/products"><ButtonIcon /></Link>
+            
           </div>
           <div className="home-image-container">
             <MainImage />
           </div>
         </div>
       </div>
-    </>
   );
 };
 
