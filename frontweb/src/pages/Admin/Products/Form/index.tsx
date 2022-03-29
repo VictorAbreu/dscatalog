fd0ce2b5265b19defa98bfd59b77d5f2fde5314c
Victor Abreu <vlabreu@outlook.com>
2022-03-29 20:17:40 -0300

    CRUD

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,13 +1,82 @@
-
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { Product } from 'types/product';
+import { requestBackend } from 'util/requests';
+import './styles.css';
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Product>();
 
-    return (
-        
-        <h1>Tela form</h1>
+  const onSubmit = (formData: Product) => {
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/products',
+      data: formData,
+      withCredentials: true,
+    };
 
-    )
+    requestBackend(config).then((response) => {
+      console.log(response.data);
+    });
+  };
 
-}
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-input-left-container">
+              <div className="margin-botton-30">
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome do produto"
+                  name="name"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.name?.message}
+                </div>
+              </div>
+              <div className="margin-botton-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div>
+                <textarea
+                  name=""
+                  rows={10}
+                  className="form-control base-input h-auto"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-button">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-button text-white">
+              SALVAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Form;
