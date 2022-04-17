387b6561dfcdaf3afb7bbae5ade6fdfd4a8053e7
Victor Abreu <vlabreu@outlook.com>
2022-04-17 14:49:28 -0300

    Refactor token, Role

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,7 +1,34 @@
 import { formatPrice } from '../formatters';
- 
-test('formatPrice should format number pt-BR when given 10.1', () => {
-  const result = formatPrice(10.1);
- 
-  expect(result).toEqual('10,10');
-});
+
+describe('formatPrice para numeros positivos', () => {
+
+  test('formatPrice should format number pt-BR when given 10.1', () => {
+    const result = formatPrice(10.1);
+
+    expect(result).toEqual('10,10');
+  });
+
+  test('formatPrice should format number pt-BR when given 0.1', () => {
+    const result = formatPrice(0.1);
+
+    expect(result).toEqual('0,10');
+  });
+
+})
+
+describe('formatPrice para numeros não-positivos', () => {
+
+  test('formatPrice should format number pt-BR when given 0', () => {
+    const result = formatPrice(0);
+
+    expect(result).toEqual('0,00');
+  });
+
+  test('formatPrice should format number pt-BR when given -5.1', () => {
+    const result = formatPrice(-5.1);
+
+    expect(result).toEqual('-5,10');
+  });
+
+})
+
