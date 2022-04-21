2b88a60a16c46fae4b3f09aa3fcef1ae218ad25b
Victor Abreu <vlabreu@outlook.com>
2022-04-20 21:02:53 -0300

    Mocking function with spyOn

---
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -0,0 +1,31 @@
+import { hasAnyRoles } from '../auth';
+import * as TokenModule from '../token';
+
+describe('hasAnyRoles tests', () => {
+
+  test('deveria retornar verdadeiro quando a lista estiver vazia', () => {
+
+    const result = hasAnyRoles([]);
+
+    expect(result).toEqual(true);
+
+  });
+
+
+  test('deveria retornar verdadeiro quando o usuario possuir role', () => {
+
+    jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+
+      exp: 0,
+      user_name: '',
+      authorities: ['ROLE_ADMIN', 'ROLE_OPERATOR']
+
+    })
+
+    const result = hasAnyRoles(['ROLE_ADMIN']);
+
+    expect(result).toEqual(true);
+
+  });
+
+});
